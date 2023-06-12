1215969083350c457c19f8ec5b0251cdd7711a63
Anthony Vaquer <anthonymark126@gmail.com>
2023-06-12 00:35:37 -0700

    finished category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,6 +20,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
 });
 
+// get category by id
 router.get("/:id", async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id);
@@ -33,25 +34,54 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-// create a new category
-  router.post('/', async (req, res) => {
-    try {
-      const categoryData = await Category.create(req.body);
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(400).json(err);
+
+// create category
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body); 
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// update cateogry by its id
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "Categoy id does not exist." });
+      return;
     }
-  });
+    res.status(200).json({ message: "Category has been updated." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
-  
-});
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+// delete category by its id
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "Categoy id does not exist." });
+      return;
+    }
+    res.status(200).json({ message: "Category has been deleted." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
